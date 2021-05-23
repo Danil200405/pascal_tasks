d92a35dadfabd52b7ff37bb8f65b88350f772259
Ivan Ubuntu20 <arigatory@gmail.com>
2021-05-23 01:12:30 -0700

    newt358

--- t358.pas
+++ t358.pas
@@ -1,24 +1,8 @@
-type
-   mas = array[0..9] of integer;
-
-
-
 function NumOfLed(n:longint):integer;
 	var 
-		res:integer;
-		a: mas;
+		res:integer = 0;
+		a: array[0..9] of integer = (6,2,5,5,4,5,6,3,7,6);
 begin
-	res:=0;
-	a[0]:=6;
-	a[1]:=2;
-	a[2]:=5;
-	a[3]:=5;
-	a[4]:=4;
-	a[5]:=5;
-	a[6]:=6;
-	a[7]:=3;
-	a[8]:=7;
-	a[9]:=6;
 	repeat 
 		res := res + a[n mod 10];
 		n := n div 10;
@@ -34,5 +18,3 @@ begin;
 	readln(n);
 	writeln(NumOfLed(n));
 end.
-
-		
